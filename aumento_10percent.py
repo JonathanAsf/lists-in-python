a4f44801b7c5321789ce310ad8bfad5b583a89db
Jonathan <90427766+JonathanAsf@users.noreply.github.com>
2023-10-23 23:28:08 -0300

    Create aumento_10percent.py

---
+++ aumento_10percent.py
@@ -0,0 +1,26 @@
+#Aumento de 10% nos produtos de categoria livro
+produtos = ['computador', 'livro', 'tablet', 'celular', 'tv', 'ar condicionado', 'alexa', 'máquina de café', 'kindle']
+
+#cada item da lista dos produtos corresponde a quantidade de vendas no mês e preço, nessa ordem
+produtos_ecommerce = [
+    [10000, 2500],
+    [50000, 40],
+    [7000, 1200],
+    [20000, 1500],
+    [5800, 1300],
+    [7200, 2500],
+    [200, 800],
+    [3300, 700],
+    [1900, 400]
+]
+
+if "livro" in produtos:
+    i_livro = produtos.index('livro')
+    antigo_total = produtos_ecommerce [i_livro] [0] * produtos_ecommerce [i_livro] [1]
+    print(f"Faturamento total antes do aumento: R${antigo_total:,}")
+    produtos_ecommerce [i_livro] [1] += produtos_ecommerce [i_livro] [1] * 0.1
+    novo_total = produtos_ecommerce [i_livro] [0] * produtos_ecommerce [i_livro] [1]
+    print(f"Faturamento total depois do aumento: R${novo_total:,}")
+    print(f"A diferença de faturamento total foi de: R${novo_total - antigo_total:,}")
+else: 
+    pass
